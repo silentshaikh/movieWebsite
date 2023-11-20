127c4d7e7697725e80984b2f6647bf6b730295c2
silentshaikh <sheikhabdulmoiz283@gmail.com>
2023-11-20 22:38:52 +0500

    update style

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -6,8 +6,7 @@ function Error() {
   return (
     <>
         <section className='errorpage'>
-          <h1>404</h1>
-            <h1>Error</h1>
+            <h1>404 Error</h1>
             <button onClick={() => errNavig('/')}>Go Back</button>
         </section>
     </>
